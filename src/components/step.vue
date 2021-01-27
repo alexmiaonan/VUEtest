94d6daa55463ff5117e781be06fdc507117797dd
miao <gao@qq.com>
2021-01-27 21:51:12 +0800

    vue element样式测试

--- src/components/step.vue
+++ src/components/step.vue
@@ -11,27 +11,26 @@
 	export default {
 		data() {
 			return {
-				
 				num2: 0,
 			}
 		},
 		beforeCreate() {
-			console.log("创建之前")
+			console.log("创建之前",this.num2)
 		},
 		created() {
-			console.log("创建了")
+			console.log("创建了",this.num2)
 		},
 		beforeMount() {
-			console.log("渲染之前")
+			console.log("渲染之前",this.num2)
 		},
 		mounted() {
-			console.log("渲染了")
+			console.log("渲染了",this.num2)
 		},
 		beforeUpdate() {
-			console.log("更新之前")
+			console.log("更新之前",this.num2,this.$el.innerText)
 		},
 		updated() {
-			console.log("更新了")
+			console.log("更新了",this.num2,this.$el.innerText)
 		},
 		beforeDestroy() {
 			console.log("销毁之前")
--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 import HiWorld from '@/components/HiWorld.vue'
 
 Vue.component("HiWorld", HiWorld)
-
+Vue.use(ElementUI);
 new Vue({
 	router,
 	store,
