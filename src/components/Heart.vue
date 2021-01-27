d604470accfe3dd21a53f47595a1b94df4f0c634
miao <gao@qq.com>
2021-01-27 10:12:39 +0800

    vue 点赞实例

---
+++ src/components/Heart.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="Heart" @click="change">
+		<br>
+		<slot name="heart"></slot>
+		<br>
+		<img :src="state?state2:state1" alt="">
+		<span>{{ state?"取消点赞":"点赞" }}</span>
+		<br>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				state: true,
+				state1:require("../assets/img/1.png"),
+				state2:require("../assets/img/2.png")
+			}
+		},
+		methods:{
+			change(){
+				this.state=!this.state
+				this.$emit("toparents",{"state":this.state})
+			},
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	.Heart {
+		display: inline-block;
+
+		img {
+			vertical-align: middle;
+		}
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,34 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js App" />
-		<HiWorld msg="home传来的msg" :num="20" @send="sendmsg"><h1 slot="shang">Home页面插槽1</h1><h2 slot="xia">Home页面插槽2</h2></HiWorld>
+		<!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+		<!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+		<!-- <HiWorld msg="home传来的msg" :num="20" @send="sendmsg"><h1 slot="shang">Home页面插槽1</h1><h2 slot="xia">Home页面插槽2</h2></HiWorld> -->
+		<p>home页面</p>
+		<Heart :stateprop="heartstate" @toparents="statechange"><span slot="heart">为你点赞！</span></Heart>
 	</div>
 </template>
 
 <script>
 	// @ is an alias to /src
-	import HelloWorld from '@/components/HelloWorld.vue'
+	// import HelloWorld from '@/components/HelloWorld.vue'
+	import Heart from "@/components/Heart.vue"
 	export default {
 		name: 'Home',
 		components: {
-			HelloWorld,
+			// HelloWorld,
+			Heart
 		},
 		methods:{
-			sendmsg(e){
-				console.log("Hiworld传输的参数",e)
+			// sendmsg(e){
+			// 	console.log("Hiworld传输的参数",e)
+			// }
+			statechange(e){
+				console.log("更改点赞状态！",e.state)
+			}
+		},
+		data(){
+			return{
+				heartstate:false,
 			}
 		}
 	}
