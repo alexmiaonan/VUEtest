83635be919be28006358d37f29104b324278c12c
miao <gao@qq.com>
2021-01-28 09:30:56 +0800

    vue 匿名导入导出和具名导入导出

---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+let obj1 = {
+	name: "gao",
+	age: 1
+}
+let obj2 = {
+	name: "chen",
+	age: 2
+}
+let obj3 = {
+	name: "miao",
+	age: 3
+}
+export default obj1
+export {
+	obj2,
+	obj3
+}
--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';Vue.use(ElementUI);
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 import HiWorld from '@/components/HiWorld.vue'
+import obj111 from "./data"
+import {
+	obj2,
+	obj3
+} from "./data/index.js"
+console.log("obj1", obj111)
+console.log("obj2", obj2, "obj3", obj3)
 
 Vue.component("HiWorld", HiWorld)
 Vue.use(ElementUI);
